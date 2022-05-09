9b30df7197fca5d35e537836b88f2096c4393c3a
Ingris Andrade <iac22@outlook.com>
2022-05-09 14:45:56 -0300

    Implementa a função getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const especie = species.find((specie) => specie.name.includes(animal));
+  return especie.residents.every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
