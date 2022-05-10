4033a91f833528b1a50a035d6abbe5110c5eb93c
Ingris Andrade <iac22@outlook.com>
2022-05-10 17:47:09 -0300

    Refatora implementação da função countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,4 +1,4 @@
-const data = require("../data/zoo_data");
+const data = require('../data/zoo_data');
 
 // contabiliza a quantidade de animais de cada espécie.
 function countAnimals(animal) {
@@ -10,9 +10,12 @@ function countAnimals(animal) {
     return totalAnimals;
   }
   if (Object.keys(animal).length === 1) {
-    return data.species.find((specie) => animal.specie === specie.name).residents.length;
+    return data.species.find((specie) => animal.specie === specie.name)
+      .residents.length;
   }
-  return data.species.find((specie) => animal.specie === specie.name).residents.filter((resident) => resident.sex === animal.sex).length;
+  return data.species
+    .find((specie) => animal.specie === specie.name)
+    .residents.filter((resident) => resident.sex === animal.sex).length;
 }
 
 console.log(countAnimals());
