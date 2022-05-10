0c36b059949271b59629546804edd26a406aa242
Ingris Andrade <iac22@outlook.com>
2022-05-10 14:09:11 -0300

    Implementa a função countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
-const data = require('../data/zoo_data');
+const data = require("../data/zoo_data");
 
+// contabiliza a quantidade de animais de cada espécie.
 function countAnimals(animal) {
-  // seu código aqui
+  const totalAnimals = {};
+  if (!animal) {
+    data.species.forEach((specie) => {
+      totalAnimals[specie.name] = specie.residents.length;
+    });
+    return totalAnimals;
+  }
+  if (Object.keys(animal).length === 1) {
+    return data.species.find((specie) => animal.specie === specie.name).residents.length;
+  }
+  return data.species.find((specie) => animal.specie === specie.name).residents.filter((resident) => resident.sex === animal.sex).length;
 }
+
+console.log(countAnimals());
 
 module.exports = countAnimals;
