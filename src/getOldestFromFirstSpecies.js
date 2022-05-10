d289580b7adc170d34aec42a636f5a316b82c6e8
Ingris Andrade <iac22@outlook.com>
2022-05-10 16:13:26 -0300

    Implementa a função getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,20 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+// busca por informações do animal mais velho da primeira espécie gerenciada pela pessoa colaboradora do parâmetro.
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  //
+  const getSpecie = employees.find((elem) => elem.id === id).responsibleFor[0];
+  const getResidents = species.find(
+    (specieAnimal) => specieAnimal.id === getSpecie, // vírgula adicionada na formatação
+  ).residents;
+  const getOldest = getResidents.reduce((older, animal) =>
+    (older.age > animal.age ? older : animal), // vígula adicionada na formatação
+  );
+  return [getOldest.name, getOldest.sex, getOldest.age];
 }
+
+console.log(getOldestFromFirstSpecies);
 
 module.exports = getOldestFromFirstSpecies;
