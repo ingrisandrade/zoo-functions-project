a023699249bb550ebe29c40768ef57ef7ac8f14d
Ingris Andrade <iac22@outlook.com>
2022-05-10 17:30:34 -0300

    Implementa a função getEmployeesCoverage

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,33 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require("../data/zoo_data");
+const data = require("../data/zoo_data");
 
-function getEmployeesCoverage() {
-  // seu código aqui
+// Esta função será responsável por associar informações de cobertura das pessoas funcionárias.
+
+const retorno = employees.map((elem) => {
+  const object = {
+    id: elem.id,
+    fullName: `${elem.firstName} ${elem.lastName}`,
+    species: elem.responsibleFor.map(
+      (respons) => species.find((search) => search.id === respons).name,
+    ),
+    locations: elem.responsibleFor.map(
+      (respons) => species.find((search) => search.id === respons).location,
+    ),
+  };
+  return object;
+});
+
+function getEmployeesCoverage(idRespons = retorno) {
+  // const idRespons = retorno;
+  if (idRespons.id || idRespons.name) {
+    const object = retorno.find((searchRespons) => searchRespons.fullName.includes(idRespons.name) || searchRespons.id === idRespons.id);
+    if (!object) {
+      throw new Error('Informações inválidas');
+    }
+    return object;
+  }
+  return idRespons;
 }
 
+// console.log(getEmployeesCoverage());
 module.exports = getEmployeesCoverage;
