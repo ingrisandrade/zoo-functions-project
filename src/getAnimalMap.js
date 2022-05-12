eb523bb2c34290421b27c4721ab72e695818ba75
Ingris Andrade <iac22@outlook.com>
2022-05-12 10:46:50 -0300

    Implementa as funções getAnimalMap e getSchedule

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,5 +1,7 @@
 const data = require('../data/zoo_data');
 
+// A função é responsável pelo mapeamento geográfico das espécies e seus animais, podendo ainda filtrá-los por ordem alfabética e sexo.
+
 function getAnimalMap(options) {
   // seu código aqui
 }
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,38 @@
 const data = require('../data/zoo_data');
 
+// A função animal dias recebe animal e os dias da semana que ele está disponível
+function animalDias(animal) {
+  return data.species.find((anim) => anim.name === animal).availability;
+}
+
+// A função diaHorarios recebe o dia e retorna o horário de funcionamento em que os animais irão se apresentar naquele dia.
+function diaHorarios(day) {
+  const apresentacao = data.species.filter((animal) => animal.availability.includes(day))
+    .map((anim) => anim.name);
+
+  const { open, close } = data.hours[day];
+
+  if (day === 'Monday') {
+    return { Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } };
+  }
+
+  return { [day]: {
+    officeHour: `Open from ${open}am until ${close}pm`,
+    exhibition: apresentacao },
+  };
+}
+
+const week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+function withoutParameter() {
+  return week.reduce((acc, dia) => Object.assign(acc, diaHorarios(dia)), {});
+}
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const nomeAnimal = data.species.map((animal) => animal.name);
+  if (week.includes(scheduleTarget)) { return diaHorarios(scheduleTarget); }
+  if (nomeAnimal.includes(scheduleTarget)) { return animalDias(scheduleTarget); }
+  return withoutParameter();
 }
 
 module.exports = getSchedule;
